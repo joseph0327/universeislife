26ceb5077577fece6145e79a2279545755c6cb54
joseph0327 <joseph.villarin0327@gmail.com>
2020-10-26 01:11:27 +0800

    deploy

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,59 @@
+import Icon1 from '../../images/image02.svg'
+import Icon2 from '../../images/image05.png'
+import Icon3 from '../../images/image06.png'
+
+
+export const homeObjOne = {
+    id:'about',
+    lightBg: false,
+    lightText: true,
+    LightTextDesc: true,
+    topLine: 'Premium Offers',
+    headline: 'You will be able to have an Unlimited Transaction with zero fees',
+    description: 'Get access to an our exlcusive app that allows you to send unlimitted transactions without gettings charged of any fees.',
+    buttonLabel: 'Learn More',
+    imgStart: false, 
+    img: Icon1,
+    alt: '',
+    dark: true,
+    primary: true,
+    darkText: false,
+
+};
+
+export const homeObjTwo = {
+    id:'discover',
+    lightBg: true,
+    lightText: false,
+    LightTextDesc: false,
+    topLine: 'Learn more',
+    headline: 'This is a newly created website ',
+    description: 'Get access to an our exlcusive app that allows you to send unlimitted transactions without gettings charged of any fees.',
+    buttonLabel: 'More...',
+    imgStart: true,
+    img: Icon2,
+    alt: '',
+    dark: false,
+    primary: false,
+    darkText: true,
+
+};
+
+
+export const homeObjThree = {
+    id:'signup',
+    lightBg: true,
+    lightText: false,
+    LightTextDesc: false,
+    topLine: 'Sign Up Now!!!',
+    headline: 'Win a million dollar cash',
+    description: 'You may now sign up and get access to an our exlcusive app.',
+    buttonLabel: 'Sign Up',
+    imgStart: false,
+    img: Icon3,
+    alt: '',
+    dark: false,
+    primary: false,
+    darkText: true,
+
+};
---
+++ src/components/pages/signin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignIn from '../Signin'
+import ScrollToTop from '../ScrollToTop'
+
+const SigninPage = () => {
+    return (
+        <>
+            <ScrollToTop />
+            <SignIn /> 
+        </>
+    )
+}
+
+export default SigninPage
